b34ef7a75ba18e2cf28f6ed81eed3db7e90e85dd
Harry Prior <harryprior@hotmail.co.uk>
2021-01-10 17:11:40 +0000

    Testing for changes in distance size

--- main.py
+++ main.py
@@ -12,20 +12,18 @@ for any coordinate x, it's square is given by: round( x - minimum / the size of 
 def main():
     lat_longs = [
         (0, 0),
-        (0.5, 0.5),
-        (0, 1),
-        (1, 0),
-        (5, 5),
-        (5, 4)
+        (0.5, 0.5)
     ]
     distance = 0.5
     normalised_lat_longs = normalise_lat_longs(lat_longs)
+    print(normalised_lat_longs)
 
     grid_info = create_grid_info_dict(normalised_lat_longs, distance)
 
     filled_grid, filled_squares = get_filled_grid_and_filled_squares(create_grid_dict(grid_info), grid_info)
 
     print(filled_squares)
+    print(filled_grid)
     print(group_by_adjacent_tiles(filled_squares, distance))
 
 
@@ -47,7 +45,7 @@ def create_grid_dict(grid_info: dict):
 
 
 def normalise_lat_longs(lat_longs: [(float, float)]) -> [(int, int)]:
-    return list(map(lambda x: (int((x[0] + 90)), int((x[1] + 180))), lat_longs))
+    return list(map(lambda x: ((x[0] + 90), (x[1] + 180)), lat_longs))
 
 
 def get_filled_grid_and_filled_squares(grid: dict, grid_info: dict) -> (dict, set):
@@ -72,7 +70,7 @@ def group_by_adjacent_tiles(filled_squares: set, distance) -> [set]:
         if current_square not in filled_squares or current_square in visited:
             return None
         visited.add(current_square)
-        nesw = [(0, distance), (distance, 0), (-distance, 0), (0, -distance)]
+        nesw = [(0, 1), (1, 0), (-1, 0), (0, -1)]
         for direction in nesw:
             new_coord = tuple(map(lambda x: x[0] + x[1], zip(direction, current_square)))
             group_set.add(current_square)
